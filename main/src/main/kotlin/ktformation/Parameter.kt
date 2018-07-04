45fedc2e0e95b5b2e1701c5f8501870f5eb3a357
IIBUN Toshiyuki <toshiyuki.iibun@gmail.com>
2018-07-04 17:31:45 +0900

    add AWS specific type and SSM type

--- main/src/main/kotlin/ktformation/Parameter.kt
+++ main/src/main/kotlin/ktformation/Parameter.kt
@@ -61,12 +61,73 @@ data class Parameter(@JsonIgnore val name: String) {
     }
 }
 
+data class ParameterType(private val typeName: String) {
+    companion object {
+        val NUMBER = ParameterType("Number")
+        val STRING = ParameterType("String")
+        val LIST_NUMBER = ParameterType("List<Number>")
+        val COMMA_DELIMITED_LIST = ParameterType("CommaDelimitedList")
 
-enum class ParameterType(private val typeName: String) {
-    NUMBER("Number"),
-    STRING("String"),
-    LIST_NUMBER("List<Number>"),
-    COMMA_DELIMITED_LIST("CommaDelimitedList");
+        /**
+         * An Availability Zone, such as us-west-2a.
+         */
+        val AWS_EC2_AVAILABILITYZONE_NAME = ParameterType("AWS::EC2::AvailabilityZone::Name")
+
+        /**
+         * An Amazon EC2 image ID, such as ami-ff527ecf. Note that the AWS CloudFormation console doesn't show a drop-down list of values for this parameter type.
+         */
+        val AWS_EC2_IMAGE_ID = ParameterType("AWS::EC2::Image::Id")
+
+        /**
+         * An Amazon EC2 instance ID, such as i-1e731a32.
+         */
+        val AWS_EC2_INSTANCE_ID = ParameterType("AWS::EC2::Instance::Id")
+
+        /**
+         * An Amazon EC2 key pair name.
+         */
+        val AWS_EC2_KEYPAIR_KEYNAME = ParameterType("AWS::EC2::KeyPair::KeyName")
+
+        /**
+         * An EC2-Classic or default VPC security group name, such as my-sg-abc.
+         */
+        val AWS_EC2_SECURITYGROUP_GROUPNAME = ParameterType("AWS::EC2::SecurityGroup::GroupName")
+
+        /**
+         * A security group ID, such as sg-a123fd85.
+         */
+        val AWS_EC2_SECURITYGROUP_ID = ParameterType("AWS::EC2::SecurityGroup::Id")
+
+        /**
+         * A subnet ID, such as subnet-123a351e.
+         */
+        val AWS_EC2_SUBNET_ID = ParameterType("AWS::EC2::Subnet::Id")
+
+        /**
+         * An Amazon EBS volume ID, such as vol-3cdd3f56.
+         */
+        val AWS_EC2_VOLUME_ID = ParameterType("AWS::EC2::Volume::Id")
+
+        /**
+         * A VPC ID, such as vpc-a123baa3.
+         */
+        val AWS_EC2_VPC_Id = ParameterType("AWS::EC2::VPC::Id")
+
+        /**
+         * An Route 53 hosted zone ID, such as Z23YXV4OVPL04A.
+         */
+        val AWS_ROUTE53_HOSTEDZONE_ID = ParameterType("AWS::Route53::HostedZone::Id")
+
+        fun list(parameterType: ParameterType) = ParameterType("List<${parameterType}>")
+
+        /**
+         * The name of a Systems Manager parameter key.
+         * Use this parameter when you want to pass the parameter key. For example, you can use this type to validate that the parameter exists.
+         */
+        val AWS_SSM_PARAMETER_NAME = ParameterType("AWS::SSM::Parameter::Name")
+
+        fun ssm(parameterType: ParameterType) = ParameterType("AWS::SSM::Parameter::Value<${parameterType}>")
+    }
 
     override fun toString() = typeName
 }
